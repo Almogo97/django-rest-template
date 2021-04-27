cceb2c97e564361bc166ad7bbe146f526e35e347
Almogo97 <alvaromg_97@hotmail.com>
2021-04-27 18:51:36 +0000

    Split settings file
    This allows to use a different settings file depending on the
    environment

--- app/settings/base.py
+++ app/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', False)
+DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
---
+++ app/settings/development.py
@@ -0,0 +1,4 @@
+from .base import * #noqa
+
+
+INSTALLED_APPS += ['django_extensions']
