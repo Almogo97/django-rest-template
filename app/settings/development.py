9c41444d2970d77086b05b15db4580417057a3c9
Almogo97 <alvaromg_97@hotmail.com>
2021-04-27 19:11:41 +0000

    Add query inspection tool

--- app/settings/development.py
+++ app/settings/development.py
@@ -2,3 +2,26 @@ from .base import * #noqa
 
 
 INSTALLED_APPS += ['django_extensions']
+
+MIDDLEWARE += (
+    'qinspect.middleware.QueryInspectMiddleware',
+)
+
+QUERY_INSPECT_ENABLED = True
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'qinspect': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
